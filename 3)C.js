e8bec4c6969b6c13401c185dad0eae756cd297fb
Sathish <115335195+sathi2536@users.noreply.github.com>
2023-03-28 20:24:43 +0530

    Add files via upload

---
+++ 3)C.js
@@ -0,0 +1,14 @@
+// sum of the arry
+        var arr=[1,2,3,4,5,6,7,8,9,10]
+        let sum = ()=>{
+            var sum =0;
+  for (var i=0;i<arr.length;i++){
+    sum+=arr[i]  
+    }
+    console.log(sum)
+    
+}  
+sum()
+
+        
+  
